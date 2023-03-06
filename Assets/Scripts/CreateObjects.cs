b5b256c56159e95d7e1737bc3ff0dc6b5fbcea8e
tioz2021 <jenya25life@gmail.com>
2023-03-06 21:40:19 +0200

    craeteObject and randomize prefabs for start

--- Assets/Scripts/CreateObjects.cs
+++ Assets/Scripts/CreateObjects.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class CreateObjects : MonoBehaviour
 {
 
-	public GameObject obj;
+	public GameObject[] obj;
 
 	void Start()
 	{
-		GameObject newObject = Instantiate(obj, new Vector3(0, 5, 0), Quaternion.Euler(12f, -15f, 40f)) as GameObject;
-		newObject.GetComponent<Transform>().position = new Vector3(5, 5, 0);
+		for (int i = 0; i < 5; i++)
+		{
+			Instantiate(obj[RandomNumber(0, obj.Length)],
+				new Vector3(RandomNumber(), RandomNumber(), RandomNumber()),
+				Quaternion.Euler(RandomNumber(), RandomNumber(), RandomNumber()));
+		}
+		// GameObject newObject = Instantiate(obj, new Vector3(0, 5, 0), Quaternion.Euler(12f, -15f, 40f)) as GameObject;
+		// newObject.GetComponent<Transform>().position = new Vector3(5, 5, 0);
+	}
+
+	private int RandomNumber(int start = 0, int end = 10)
+	{
+		return Random.Range(start, end);
 	}
 
 }
