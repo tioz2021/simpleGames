90f2fe51c49c12c8c9f5098deb1a61266f51cf09
tioz2021 <jenya25life@gmail.com>
2023-03-06 21:49:32 +0200

    create EnableLight and toggle light

---
+++ Assets/Scripts/EnableLight.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnableLight : MonoBehaviour
+{
+	public Light _mainLight;
+
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			_mainLight.enabled = !_mainLight.enabled;
+		}
+	}
+}
